88e3baac6b5a40d853491f0fd4cf95e3c4e799da
prem36999 <premdevops369@gmail.com>
2026-06-21 13:13:30 +0530

    shell script added

--- 03-shell-script.sh
+++ 03-shell-script.sh
@@ -6,4 +6,4 @@ PERSON2=Suresh
 echo "$PERSON1:: hai $PERSON2"
 echo "$PERSON2:: hello $PERSON1 hOw are u"
 echo "$PERSON1:: i am fine $PERSON2 what about u"
-echo "$PERSON2:: fine $PERSON1
+echo "$PERSON2:: fine $PERSON1"
