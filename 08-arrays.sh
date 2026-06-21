9cb7d8353ba93c9b66f91bfccb379af7847d3cdc
prem36999 <premdevops369@gmail.com>
2026-06-21 14:30:57 +0530

    shell script added

---
+++ 08-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("Ppushpa" "rrr" "devara")
+echo "firsr movie: ${MOVIES[0]}"
+echo "second movie: ${MOVIES[1]}"
+echo "thared movie; ${MOVIES[2]}"
+
+echo "All movies are: ${MOVIES[@]}"
