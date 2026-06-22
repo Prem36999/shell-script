4f3548061972c243cbb52ec3acac3d4e64209151
prem36999 <premdevops369@gmail.com>
2026-06-22 09:57:43 +0530

    shell script added

--- 11-install-script.sh
+++ 11-install-script.sh
@@ -4,11 +4,24 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR:: you must have sudo access to execute thescript"
+    echo "ERROR:: you must have sudo access to execute thescript"  
+    exit 1 #other than 0
+fi
 
-fi  
-exit 1 #other than 0
-
-dnf install mysql -y
+dnf install mysqli -y
+if [ $? -ne 0 ]
+then 
+   echo "installing MSQL ...   FAILURE"
+   exit 1   
+else 
+   echo "installing MYQL...  SUCCESS"
+fi
 
 dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "installing GIT ...  FAILURE"    
+    exit 1 
+else
+    echo "installing GIT... SUCCESS" 
+fi       
