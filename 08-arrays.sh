3521ff5941fee990e855a36506b2d51d850c246a
prem36999 <premdevops369@gmail.com>
2026-06-28 17:05:22 +0530

    shell script added

--- 08-arrays.sh
+++ 08-arrays.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-MOVIES=("Ppushpa" "rrr" "devara")
-echo "firsr movie: ${MOVIES[0]}"
+MOVIES=("Ppushpa" "rrr" "devara" "kushi" "prema")
+echo "first movie: ${MOVIES[0]}"
 echo "second movie: ${MOVIES[1]}"
 echo "thared movie; ${MOVIES[2]}"
+echo "fourth movie; ${MOVIES[3]}"
+echo "fifth movie; ${MOVIES[4]}"
+
 
 echo "All movies are: ${MOVIES[@]}"
