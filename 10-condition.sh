4e82c26dbae246fded40ebc941f4ccfc903bcea8
prem36999 <premdevops369@gmail.com>
2026-06-28 17:40:35 +0530

    shell script added

--- 10-condition.sh
+++ 10-condition.sh
@@ -20,3 +20,10 @@ then
 else 
   echo -e  "ravi is $R failed $N "
 fi
+
+JOB=$3
+if [ $JOB -gt 75 ]
+then
+   echo -e "prem got the job $G sucuussully $N "
+else
+  echo -e "prem did not got the job $Y hold on $N "   
