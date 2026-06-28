3302e4e6a2f0fa1360934b2398ffef697acf0a68
prem36999 <premdevops369@gmail.com>
2026-06-28 16:55:43 +0530

    shell script added

--- 07-data-type.sh
+++ 07-data-type.sh
@@ -5,11 +5,11 @@ NUMBER2=$2
 
 TIMESTAMP=$(date)
 echo "script ececuted at:: $TAIMESTAMP"
-SUM=$(($NUMBER3+$NUMBER2))
+SUM=$(($NUMBER3*$NUMBER2))
 echo "SUM of $NUMBER3 and $NUMBER2 is:: $SUM"
 
 
-A=5000
-B=10
-SUM=$((A/B))
-echo "SUM =$SUM"
+A=$1
+B=$2
+SUM=$((A+B))
+echo "SUM OF A AND B IS :: =$SUM"
