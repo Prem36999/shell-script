3a003b83e699e2249f784321fc9844514b27ef3e
prem36999 <premdevops369@gmail.com>
2026-06-29 15:50:26 +0530

    shell script added

---
+++ 15-loops.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){ 
+    if [ $1 -ne 0 ]
+  then
+     echo -e "$2 ... $R FAILURE $N "
+     exit 1
+  else
+     echo -e  "$2... $G SUCCESS $N "
+  fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to execute the script"  
+    exit 1 #other than 0
+fi
+
+for package in $@
+do
+ dnf list installed $package
+ if [ $? -ne 0 ]
+ then
+ dnf install $package -y 
+ VALIDATE $? "inatalling $package "
+ else
+ echo -e "$package is allrady $Y.... INSTALLLED $N"
+done
