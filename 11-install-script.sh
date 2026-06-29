96708594ec81205ed1b99ebd75455e86cfabe5b7
prem36999 <premdevops369@gmail.com>
2026-06-29 12:10:34 +0530

    shell script added

--- 11-install-script.sh
+++ 11-install-script.sh
@@ -6,7 +6,6 @@ if [ $USERID -ne 0 ]
 then
     echo "ERROR:: you must have sudo access to execute the script"  
     exit 1 #other than 0
-fi
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
---
+++ 12-function.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+PREM(){
+    if [ $1 -ne 0 ]
+  then
+     echo  "$2 ... FAILURE  "
+     exit 1
+  else
+     echo  "$2... SUCCESS  "
+  fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to execute the script"  
+    exit 1 #other than 0
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+  dnf install mysql -y
+PREM $? "installing MSQL" 
+else
+   echo  "MYSQL is Allredy installed "
+fi
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+  dnf install git -y
+ PREM $? "installing GIT" 
+else 
+  echo "GIT is allredy installed"
+fi  
+
+
