1a48822f60b56e829650ce430ba54c48c2d6ad32
prem36999 <premdevops369@gmail.com>
2026-06-29 14:42:16 +0530

    shell script added

--- 14-logs.sh
+++ 14-logs.sh
@@ -13,14 +13,11 @@ LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 PREM(){ 
     if [ $1 -ne 0 ]
   then
-     echo -e "$2 ... $R FAILURE $N "&>>LOG_FILE_NAME
-     exit 1
-  else
-     echo -e  "$2... $G SUCCESS $N "&>>LOG_FILE_NAME
+     echo -e "$2 ... $R FAILURE $N "&>>$LOG_FILE_NAME
   fi
 }
 
-echo "script started executing at: $TIMESTAMP" &>>LOG_FILE_NAMDE
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAMDE
 
 if [ $USERID -ne 0 ]
  then
@@ -29,22 +26,22 @@ if [ $USERID -ne 0 ]
 fi
 
 
-dnf list installed mysql &>>LOG_FILE_NAME
+dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
  then
-  dnf install mysql -y &>>LOG_FILE_NAME
+  dnf install mysql -y &>>$LOG_FILE_NAME
   PREM $? "installing MSQL" 
 else
-   echo -e $Y  "MYSQL is Allredy installed $N " &>>LOG_FILE_NAME
+   echo -e $Y  "MYSQL is Allredy installed $N " &>>$LOG_FILE_NAME
 fi
 
 
-dnf list installed git  &>>LOG_FILE_NAME
+dnf list installed git  &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
  then
-  dnf install git -y &>>LOG_FILE_NAME
+  dnf install git -y &>>$LOG_FILE_NAME
   PREM $? "installing GIT" 
 else 
   echo -e $Y "GIT is allredy installed $N  " &>>LOG_FILE_NAME
