68767f84e657257fb6efb8091ca91113bee54245
prem36999 <premdevops369@gmail.com>
2026-06-30 10:42:13 +0530

    shell script added

--- 14-logs.sh
+++ 14-logs.sh
@@ -11,7 +11,7 @@ LOG_FILE=$(echo $0 | cat -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-PREM(){ 
+VALIDATE(){ 
     if [ $1 -ne 0 ]
   then
      echo -e "$2 ... $R FAILURE $N "
@@ -22,19 +22,19 @@ echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 if [ $USERID -ne 0 ]
 
- then
+then
     echo "ERROR:: you must have sudo access to execute the script"  
     exit 1 #other than 0
 fi
 
 
 dnf list installed mysql &>>$LOG_FILE_NAME
-
+ 
 if [ $? -ne 0 ]
 
- then
+then
   dnf install mysql -y &>>$LOG_FILE_NAME
-  PREM $? "installing MSQL" 
+  VALIDATE $? "installing MSQL" 
 else
    echo -e   "MYSQL is Allredy $Y INSTALLED $N "
 fi
@@ -44,10 +44,10 @@ dnf list installed git  &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 
- then
+then
   dnf install git -y &>>$LOG_FILE_NAME
 
-  PREM $? "installing GIT" 
+  VALIDATE $? "installing GIT" 
 else 
   echo -e  "GIT is allredy $Y INSTALLED $N  " 
 fi
