02043cc4d6cb95d0b08fc703a4f6a29700a0abde
prem36999 <premdevops369@gmail.com>
2026-07-07 13:25:25 +0530

    shell script added

--- 10-condition.sh
+++ 10-condition.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 NUMBER=$1
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 # -gt , -lt , -eq , -ge , -le
 if [ $NUMBER -gt 100 ]
 then 
-  echo  "given number is greatar than  100 "
+  echo -e "given number is $G greatar than  100 $N "
 else
-  echo  "given number is less than or equal to  100  "
+  echo  "given number is $Y less than or equal to  100 $N "
 fi    
 
 MARKS=$2
 if [ $MARKS -ge 35 ]
 then
-  echo "ravi is   passed  "
+  echo -e "ravi is  $G passed $N "
 else 
-  echo  "ravi is  failed  "
+  echo -e "ravi is $R  failed $N "
 fi
 
 MARKS=$3
 if [ $MARKS -ge 90 ]
 then
-  echo " A grade"
+  echo -e " $G A grade $N"
   elif [ $MARKS -ge 75 ]
   then 
-   echo "B grade"
+   echo -e "$Y B grade $N"
    else 
    echo " C grade"
   fi
